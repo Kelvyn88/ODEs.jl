17d11e242e4c869b4394613476e2ad7dacae1618
Kelvyn Baruc SS <kelvyn.baruc@gmail.com>
2016-06-06 22:57:16 -0500

    rk5 updated

--- src/rk5.jl
+++ src/rk5.jl
@@ -1,7 +1,7 @@
 function rk5(f, tspan, y0; h = 1, iter = false)
-#Runge Kutta 5th order
+# Runge Kutta 5th order
 t = floor((tspan[end] - tspan[1])/h)
-y = zeros(int64(t) + 1,1)
+y = zeros(Int64(t) + 1, length(y0))
 y[1,1] = y0
 j = 1
 
@@ -16,14 +16,14 @@ for i=tspan[1]:h:(tspan[end]-h)
 
       if iter == true
             if j == 1
-                  println("Iter\t\t X\t\t\t Y")
-                  @printf("%i\t\t %f\t\t %f\n",0,i,y[1,1])
-                  @printf("%i\t\t %f\t\t %f\n",j,i+h,y[j+1,1])
+                  println("Iter\t\t t\t\t\t y")
+                  @printf("%i\t\t %f\t\t %f\n",0,i,y[1])
+                  @printf("%i\t\t %f\t\t %f\n",j,i+h,y[j+1])
             else
-                  @printf("%i\t\t %2f\t\t %f\n",j,i+h,y[j+1,1])
+                  @printf("%i\t\t %2f\t\t %f\n",j,i+h,y[j+1])
             end
       end
       j += 1
 end
-return y, tspan[1]:h:tspan[end]
+return tspan[1]:h:tspan[end], y
 end
