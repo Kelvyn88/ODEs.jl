fe7463c84a3067c6fd6dec64be21ea3103b72cc0
Kelvyn Baruc SS <kelvyn.baruc@gmail.com>
2016-06-11 17:47:32 -0500

    bug fixed

--- src/rk5.jl
+++ src/rk5.jl
@@ -1,22 +1,33 @@
+
+@doc """
+      Fifth-Order Runge-Kutta Method
+      Supported by:
+      -PhD Kelvyn B. Sánchez kelvyn.baruc@gmail.com
+      -MSc Lorena S. Galicia lorys92@gmail.com
+ """ ->
+
 function rk5(f, tspan, y0; h = 1)
-# Runge Kutta 5th order
-t = floor((tspan[end] - tspan[1])/h)
-y = zeros(Int64(t) + 1, length(y0))
+tint = floor((tspan[end] - tspan[1])/h) + 1
+t = linspace(tspan[1], tspan[end],tint)
+y = zeros(Int(tint), length(y0))
 y[1,:] = y0
-j = 1
 
-for i=tspan[1]:h:(tspan[end]-h)
-      for g=1:1:length(y0)
-            k1 = f(i,y[j,:])[g]
-            k2 = f(i + (1/4)*h,y[j,:] + (1/4)*k1*h)[g]
-            k3 = f(i + (1/4)*h,y[j,:] + (1/8)*k1*h + (1/8)*k2*h)[g]
-            k4 = f(i + (1/2)*h,y[j,:] - (1/2)*k2*h + k3*h)[g]
-            k5 = f(i + (3/4)*h,y[j,:] + (3/16)*k1*h + (9/16)*k4*h)[g]
-            k6 = f(i + h,y[j,:] - (3/7)*k1*h + (2/7)*k2*h + (12/7)*k3*h - (12/7)*k4*h + (8/7)*k5*h)[g]
-            y[j+1,g] = y[j,g] + (1/90)*(7*k1 + 32*k3 + 12*k4 + 32*k5 + 7*k6)*h
+      try
+            f(t,y0)
+
+            for i=1:1:(length(t) - 1)
+                        k1 = f(t[i],y[i,:])
+                        k2 = f(t[i] + (1/4)*h,y[i,:]' + (1/4)*k1*h)
+                        k3 = f(t[i] + (1/4)*h,y[i,:]' + (1/8)*k1*h + (1/8)*k2*h)
+                        k4 = f(t[i] + (1/2)*h,y[i,:]' - (1/2)*k2*h + k3*h)
+                        k5 = f(t[i] + (3/4)*h,y[i,:]' + (3/16)*k1*h + (9/16)*k4*h)
+                        k6 = f(t[i] + h,y[i,:]' - (3/7)*k1*h + (2/7)*k2*h + (12/7)*k3*h - (12/7)*k4*h + (8/7)*k5*h)
+                        y[i+1,:] = y[i,:]' + (1/90)*(7*k1 + 32*k3 + 12*k4 + 32*k5 + 7*k6)*h
+            end
+
+             t, y;
+      catch
+            error("The number of initial values does not match the number of equations")
       end
-      j += 1
-end
 
-return tspan[1]:h:tspan[end], y
 end
